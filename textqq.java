ee56841f6f7a30d2007b76f4929227f9f8284ed8
pattadon <35373509+pattdon@users.noreply.github.com>
2019-08-11 20:44:53 +0700

    5555

---
+++ textqq.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author patta
+ */
+public class textqq {
+       public void output2(){
+        System.out.println("secend");
+    } 
+}
+
+
+
